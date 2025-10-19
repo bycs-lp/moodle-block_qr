2363cecfe61bf3bfa51772705b52da514f409358
thomas.schoenlein <schoenlein@frobenius-gymnasium.de>
2025-10-19 22:01:54 +0200

    MBS-10363: Rework template usage in block_qr

--- settings.php
+++ settings.php
@@ -14,17 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-
-
-/**
- * Admin settings for block_qr
- *
- * @package    block_qr
- * @copyright  2023 ISB Bayern
- * @author     Florian Dagner <florian.dagner@outlook.de>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 defined('MOODLE_INTERNAL') || die();
 
 global $ADMIN, $settings;
