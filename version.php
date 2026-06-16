5581a39aee1474f0b03478f2f8b44ceee9910595
PhMemmel <65113153+PhMemmel@users.noreply.github.com>
2026-06-16 06:29:15 +0200

    Bump version for initial release

--- version.php
+++ version.php
@@ -25,8 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = 'v0.0.1';
-$plugin->version   = 2026052100;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = '0.1';
+$plugin->version   = 2026061600;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041200;         // Requires this Moodle version.
+$plugin->supported = [405, 502];
 $plugin->component = 'block_qr';         // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_BETA;      // This is considered as ready for production sites.
